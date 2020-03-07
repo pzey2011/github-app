1ddaeda4e3de1cf6eda092c786fd046555dbc071
pzey2011 <peymanzeynali@yahoo.com>
2020-03-07 07:37:43 +0330

    remove logger

--- src/js/store.js
+++ src/js/store.js
@@ -1,9 +1,7 @@
 
 
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger'
 import rootReducer from "./reducers/rootReducer";
-const store = createStore(rootReducer,
-    applyMiddleware(logger));
+const store = createStore(rootReducer);
 
 export default store;
